3a6273dad6b49bc21f3e17b977adfd0ab9d12768
Kevin Smith <kevin@hearsayinteractive.com>
2016-08-06 00:39:12 -0500

    Add linting and coding standards checks to build process.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -5,8 +5,11 @@ import webpack from 'webpack-stream';
 import sourcemaps from 'gulp-sourcemaps';
 import rename from 'gulp-rename';
 import uglify from 'gulp-uglify';
+import jscs from 'gulp-jscs';
+import jshint from 'gulp-jshint';
+import notify from 'gulp-notify';
 
-gulp.task('default', () => {
+gulp.task('default', ['jscs', 'lint'], () => {
   return gulp.src('src/index.js')
     .pipe(webpack(require('./webpack.config.js')))
     .pipe(gulp.dest('./dist'))
@@ -20,4 +23,58 @@ gulp.task('default', () => {
     .pipe(rename('user-location.min.js'))
     .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest('./dist'));
+});
+
+/**
+ * Check JS files' adherence to coding standards
+ */
+gulp.task('jscs', () => {
+  gulp.src('src/**/*.js')
+    .pipe(jscs())
+    .on('error', function (err) {
+      console.log('Error:', err.message);
+      this.emit('end');
+    })
+    .pipe(notify({
+      title: 'JSCS',
+      message: function (file) {
+        if (file.jscs.success) {
+          // Don't show anything if success
+          return false;
+        }
+
+        var errors = file.jscs.errors.map(function (data) {
+          return "(" + data.line + ':' + data.column + ') ' + data.message;
+        }).join("\n");
+        return file.relative + " (" + file.jscs.errors.length + (file.jscs.errors.length > 1 ? " errors" : " error") + ")\n" + errors;
+      },
+      sound: 'Pop'
+    }));
+});
+
+
+/**
+ * Check JS files for errors and potential problems
+ */
+gulp.task('lint', () => {
+  gulp.src('src/**/*.js')
+    .pipe(jshint('.jshintrc'))
+    .pipe(notify({
+      title: 'JSHint',
+      message: function (file) {
+        if (file.jshint.success) {
+          // Don't show anything if success
+          return false;
+        }
+
+        var errors = file.jshint.results.map(function (data) {
+          if (data.error) {
+            return "(" + data.error.line + ':' + data.error.character + ') ' + data.error.reason;
+          }
+        }).join("\n");
+        return file.relative + " (" + file.jshint.results.length + (file.jshint.results.length > 1 ? " errors" : " error") + ")\n" + errors;
+      },
+      sound: 'Pop'
+    }))
+    .pipe(jshint.reporter('jshint-stylish'))
 });
