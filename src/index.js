07531abf59d97f41f39bb88dc5f5803646532451
Kevin Smith <kevin@hearsayinteractive.com>
2016-08-08 07:17:11 -0500

    Use the basic native gelocation API.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,31 @@
-export default class {
+export default class UserLocation {
   constructor({
     apiKey,
     cacheTtl = 604800, // 7 days
-    fallback = 'exact',
+    fallback = 'exact', // If IP-based geolocation fails
     specificity = 'general',
   }) {
-    console.log(apiKey, cacheTtl, fallback, specificity);
+    let coords = {};
+
+    if (specificity === 'exact') {
+      navigator.geolocation.getCurrentPosition(
+        (pos) => {
+          console.log(pos.coords);
+        },
+        (err) => {
+          throw new Error(`${err.message} (error code: ${err.code})`);
+        }
+      );
+    } else if (specificity === 'general') {
+      // Use GeoIP lookup to get general area
+    } else {
+      throw new Error('Invalid configuration value for location specificity.');
+    }
+
+    coords = 'something';
+
+    console.log(apiKey, cacheTtl, fallback, coords);
+
+    return coords;
   }
 }
